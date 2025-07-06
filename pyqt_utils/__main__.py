6f0c280c66ccd8c7a1487eb0bfaa043d151a74db
Dominik <dominik.reinartz@proton.me>
2025-07-06 16:44:09 +0200

    Added nuitka build support

--- pyqt_utils/__main__.py
+++ pyqt_utils/__main__.py
@@ -1,7 +1,8 @@
 import argparse
+import os
 import platform
 from pathlib import Path
-from subprocess import getoutput
+from textwrap import dedent
 
 
 def _find_executable(name: str) -> str:
@@ -71,6 +72,58 @@ def main() -> None:
         help="Compile all .ts files in the langs/ directory to .qm files.",
     )
 
+    parser.add_argument(
+        "--build-linux",
+        action="store_true",
+        dest="build_linux",
+        help="Build the Linux binary using Nuitka.",
+    )
+
+    parser.add_argument(
+        "--build-windows",
+        action="store_true",
+        dest="build_windows",
+        help="Build the Windows executable using Nuitka.",
+    )
+
+    parser.add_argument(
+        "--build-macos",
+        action="store_true",
+        dest="build_macos",
+        help="Build the MacOS binary using Nuitka.",
+    )
+
+    parser.add_argument(
+        "--build-product-name",
+        action="store",
+        dest="product_name",
+        help="Set the product name for the Nuitka build.",
+    )
+
+    parser.add_argument(
+        "--build-icon",
+        action="store",
+        dest="icon_path",
+        help="The path to an icon file for the built binary file. Should be "
+             "a .png file for Linux and MacOS and a .ico file for Windows.",
+    )
+
+    parser.add_argument(
+        "--build-data-dir",
+        action="append",
+        dest="data_dirs",
+        help="Add a data dir for the Nuitka build. Uses the format "
+             "source=dest.",
+    )
+
+    parser.add_argument(
+        "--build-data-file",
+        action="append",
+        dest="data_files",
+        help="Add a data file for the Nuitka build. Uses the format "
+             "source=dest.",
+    )
+
     parser.set_defaults(func=lambda _: parser.print_help())
 
     args = parser.parse_args()
@@ -86,7 +139,7 @@ def main() -> None:
             if not ui_file.is_file():
                 continue
             print(
-                getoutput(
+                os.system(
                     f"{_find_executable("pyuic6")} {ui_file} -o "
                     f"{ui_file.with_suffix('.py').with_stem(
                         ui_file.stem + "_ui"
@@ -97,7 +150,7 @@ def main() -> None:
 
     if args.compile_icons:
         print(
-            getoutput(
+            os.system(
                 f"rcc --generator python {package}/icons/icons.qrc -o "
                 f"{package}/icons/resource.py"
             ),
@@ -118,7 +171,7 @@ def main() -> None:
             if not ts_file.is_file():
                 continue
             print(
-                getoutput(
+                os.system(
                     f"{_find_executable("lupdate")} -tr-function-alias "
                     f"translate=tr {lupdate_files} {package}/ui/ -ts "
                     f"{ts_file} -no-obsolete -source-language en_US"
@@ -131,6 +184,63 @@ def main() -> None:
             if not ts_file.is_file():
                 continue
             print(
-                getoutput(f"{_find_executable("lrelease")} {ts_file}"),
+                os.system(f"{_find_executable("lrelease")} {ts_file}"),
                 end="",
             )
+
+    build_command = dedent(
+        f"""nuitka \
+        --standalone \
+        --onefile \
+        --python-flag="no_asserts" \
+        --python-flag="no_docstrings" \
+        --python-flag="-m" \
+        --main="{package.name}" \
+        --prefer-source-code \
+        --output-dir="build/" \
+        --enable-plugin=pyqt6 \
+        """  # noqa
+    )
+    if (package / "styles").exists():
+        build_command += f"--include-data-dir=\"{package}/styles/=styles/\" "
+    if (package / "icons").exists():
+        build_command += f"--include-data-dir=\"{package}/icons/=icons/\" "
+    if (package / "langs").exists():
+        build_command += f"--include-data-dir=\"{package}/langs/=langs/\" "
+    if (package / "ui").exists():
+        build_command += f"--include-data-dir=\"{package}/ui/=ui/\" "
+    if (package / "licenses").exists():
+        build_command += f"--include-data-dir=\"{package}/licenses/=licenses/\" "  # noqa
+    if (package / "version.txt").exists():
+        build_command += f"--include-data-file=\"{package}/version.txt=version.txt\" "  # noqa
+        build_command += f"--product-version=\"$(cat {package}/version.txt)\" "
+        build_command += f"--file-version=\"$(cat {package}/version.txt)\" "
+    if args.product_name:
+        build_command += f"--product-name=\"{args.product_name}\" "
+    for data_dir in args.data_dirs or []:
+        build_command += f"--include-data-dir=\"{data_dir}\" "
+    for data_file in args.data_files or []:
+        build_command += f"--include-data-file=\"{data_file}\" "
+    if platform.system() == "Windows":
+        build_command = build_command.replace("cat", "Get-Content")
+
+    if args.build_linux:
+        command = build_command
+        if args.icon_path:
+            command += f"--linux-icon=\"{args.icon_path}\" "
+        print(os.system(command), end="")
+
+    if args.build_windows:
+        command = build_command
+        command += "--windows-console-mode=\"attach\" "
+        if args.icon_path:
+            command += f"--windows-icon-from-ico=\"{args.icon_path}\" "
+        print(os.system(command), end="")
+
+    if args.build_macos:
+        command = build_command
+        if args.product_name:
+            command += f"--macos-app-name=\"{args.product_name}\" "
+        if args.icon_path:
+            command += f"--macos-app-icon=\"{args.icon_path}\" "
+        print(os.system(command), end="")
