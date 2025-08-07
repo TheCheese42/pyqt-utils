29e0719f46ef87f8c7b71e6ef1ce1d6e3916cb4b
Dominik <dominik.reinartz@proton.me>
2025-08-07 16:09:58 +0200

    Build no longer fails when the version.txt file has trailing newlines

--- pyqt_utils/version.py
+++ pyqt_utils/version.py
@@ -3,5 +3,7 @@ try:
 except ImportError:
     from paths import VERSION_PATH  # type: ignore[no-redef]
 
-version_string = VERSION_PATH.read_text(encoding="utf-8").strip()
+version_string = VERSION_PATH.read_text(
+    encoding="utf-8"
+).splitlines()[0].strip()
 __version__: tuple[int, ...] = tuple(map(int, version_string.split(".")))
